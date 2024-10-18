0244fd5a21b4c03466e4a00dba2c888607d72138
AndreXP1 <suporteandre02@gmail.com>
2024-10-18 18:29:06 -0300

    Ultra minor "fixes" :)

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
-public class LookAt2D_v2 : MonoBehaviour
+public class GunRotate : MonoBehaviour
 {
     public float offset;
     public SpriteRenderer sprRen;
+
+
     void Start(){
-        sprRen = GetComponent<SpriteRenderer>();
-        sprRen.flipX = true; // a bundinha da arma tava olhando pro mouse e não a ponta dela ent fiz essa gambiarra (boa sorte kayk, arruma ai pls).
+
+      sprRen = GetComponent<SpriteRenderer>();
+      sprRen.flipX = true; // a bundinha da arma tava olhando pro mouse e não a ponta dela ent fiz essa "gambiarra" (boa sorte kayk, arruma ai pls).
     }
 
     void Update () {
-		//Weapon Rotation based on mouse position on screen.
-		Vector3 mousePos = Input.mousePosition;
-		mousePos.z = 5.23f;
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // random goofy aah math
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
-        if (rotZ < 89 && rotZ > -89) //Check rotation and flips gun sprite, so it doesn't stay upside down.
-        {
-            sprRen.flipY = false;
-        }
-        else
-        {
-            sprRen.flipY = true;
-        }
-    }
+      //Weapon Rotation based on mouse position on screen.
+      Vector3 mousePos = Input.mousePosition;
+      mousePos.z = 5.23f;
+
+      Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+      float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // random goofy aah math
+      transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+      if (rotZ < 89 && rotZ > -89){ //Check rotation and flips gun sprite, so it doesn't stay upside down.
+          sprRen.flipY = false;
+      }
+      else{
+          sprRen.flipY = true;
+      }
+  }
 }
